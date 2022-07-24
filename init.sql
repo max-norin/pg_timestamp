003ad816e5699104f48cc4f7efdf8e0dcd7f1c57
Max <79068976350@yandex.ru>
2022-07-24 22:52:23 +0400

    init

---
+++ init.sql
@@ -0,0 +1,7 @@
+-- Chapter 40. Event Triggers - https://postgresql.org/docs/current/event-triggers.html
+-- Event Trigger Functions - https://postgresql.org/docs/current/functions-event-triggers.html
+-- Event Trigger Firing Matrix - https://postgresql.org/docs/current/event-trigger-matrix.html
+CREATE EVENT TRIGGER "add_triggers_from_timestamp_parent_tables" ON ddl_command_end
+    WHEN TAG IN ('CREATE TABLE', 'ALTER TABLE')
+        EXECUTE PROCEDURE event_trigger_add_triggers_from_timestamp_parent_tables ();
+
