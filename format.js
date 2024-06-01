fb70c3da4e71335b5be9e839cf04fd4a7eec5a3a
max_norin <79068976350@yandex.ru>
2024-06-01 17:06:19 +0300

    new version build

--- format.js
+++
@@ -1,7 +0,0 @@
-import get_files from './get_files.js'
-import { exec } from 'child_process'
-
-const files = get_files('.')
-exec('npm run pg_format ' + files.join(' '))
-console.log('pg_format files:')
-console.log(files)
